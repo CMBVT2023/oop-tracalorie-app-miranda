bcedd3f0a7e5a1b13e20e824b77661809a3e62fe
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-04-09 12:19:44 -0700

    Added a remove feature for workouts and meals.

--- js/app.js
+++ js/app.js
@@ -29,6 +29,27 @@ class CalorieTracker {
         this._render();
     };
 
+    removeMeal(id) {
+        const index = this._meals.findIndex((meal) => meal.id === id);
+
+        if (index !== -1) {
+            const meal = this._meals[index];
+            this._totalCalories -= meal.calories;
+            this._meals.splice(index, 1);
+            this._render();
+        };
+    };
+
+    removeWorkout(id) {
+        const index = this._workouts.findIndex((workout) => workout.id === id);
+
+        if (index !== -1) {
+            const workout = this._workouts[index];
+            this._totalCalories += workout.calories;
+            this._workouts.splice(index, 1);
+            this._render();
+        };
+    };
 
     // Private Methods
 
@@ -171,6 +192,8 @@ class App {
 
         document.getElementById('meal-form').addEventListener('submit', this._newItem.bind(this, 'meal'));
         document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'));
+        document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'));
+        document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'));
     };
 
     _newItem(type, e) {
@@ -201,6 +224,20 @@ class App {
             toggle: true
         });
     };
+
+    _removeItem(type, e) {
+        e.preventDefault();
+
+        if (e.target.classList.contains('delete') || e.target.classList.contains('fa-xmark')) {
+            if (confirm('Are you sure?')) {
+                const id = e.target.closest('.card').getAttribute('data-id');
+                
+                type === 'meal' ? this._tracker.removeMeal(id) : this._tracker.removeWorkout(id);
+
+                e.target.closest('.card').remove();
+            }
+        }
+    }
 };
 
 
